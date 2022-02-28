c171cada6a6ecfb0ceeb3a7622981bcbe49d35e6
Sanghun Yun <ysn6514@gmail.com>
2022-02-28 21:11:53 +0900

    group function

--- no_53.sql
+++ no_53.sql
@@ -1,14 +1,49 @@
+-- Q: 다음중 오류가 발생하는 쿼리는?
+
+-- 1
+SELECT 회원ID, SUM(주문금액) AS 합계
+FROM 주문
+GROUP BY 회원ID
+HAVING COUNT(*) > 1;
+
+-- 2
+SELECT SUM(주문금액) AS 합계
+FROM 주문
+HAVING AVG(주문금액) > 100;
+
+-- 3
+SELECT 메뉴ID, 사용유형코드, COUNT(*) AS CNT
+FROM 시스템사용이력
+WHERE 사용일시 BETWEEN SYSDATE - 1 AND SYSDATE 
+GROUP BY 메뉴ID, 사용유형코드
+HAVING 메뉴ID = '3' AND 사용유형코드 = 100;
+
+-- 4
+SELECT 메뉴ID, 사용유형코드, AVG(COUNT(*)) AS AVGCNT
+FROM 시스템사용이력
+GROUP BY 메뉴ID, 사용유형코드;
+
+-- 정답:
+-- 4번 : not a single-group group function
+
+--------------------------------------------------------
 -- 데이터 생성
 CREATE TABLE 주문 (
     회원ID VARCHAR2(3) NOT NULL,
-    주문금액 NUMBER(10) NOT NULL,
-    CONSTRAINT PK_USER_ID PRIMARY KEY(회원ID)
+    주문금액 NUMBER(10) NOT NULL
 );
 
 INSERT ALL
     INTO 주문 (회원ID, 주문금액) VALUES ('001', 1500)
     INTO 주문 (회원ID, 주문금액) VALUES ('002', 2000)
-    INTO 주문 (회원ID, 주문금액) VALUES ('003', 3000)
+    INTO 주문 (회원ID, 주문금액) VALUES ('002', 2100)
+    INTO 주문 (회원ID, 주문금액) VALUES ('002', 3000)
+    INTO 주문 (회원ID, 주문금액) VALUES ('003', 1000)
+    INTO 주문 (회원ID, 주문금액) VALUES ('004', 800)
+    INTO 주문 (회원ID, 주문금액) VALUES ('004', 3700)
+    INTO 주문 (회원ID, 주문금액) VALUES ('005', 500)
+    INTO 주문 (회원ID, 주문금액) VALUES ('005', 1000)
+    INTO 주문 (회원ID, 주문금액) VALUES ('006', 2000)
 SELECT * FROM DUAL;
 
 CREATE TABLE 시스템사용이력 (
@@ -31,33 +66,13 @@ INSERT ALL
     INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 100, SYSDATE-1)
     INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 100, SYSDATE-1)
     INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 110, SYSDATE-1)
+    INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 100, SYSDATE)
+    INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 100, SYSDATE)
+    INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 100, SYSDATE)
     INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 120, SYSDATE)
     INTO 시스템사용이력 (메뉴ID, 사용유형코드, 사용일시) VALUES ('3', 130, SYSDATE-1)
 SELECT * FROM DUAL;
 
-DROP TABLE 시스템사용이력;
 
--- 1
-SELECT 회원ID, SUM(주문금액) AS 합계
-FROM 주문
-GROUP BY 회원ID
-HAVING COUNT(*) > 1;
-
--- 2
-SELECT SUM(주문금액) AS 합계
-FROM 주문
-HAVING AVG(주문금액) > 100;
-
--- 3
-SELECT 메뉴ID, 사용유형코드, COUNT(*) AS CNT
-FROM 시스템사용이력
-WHERE 사용일시 BETWEEN SYSDATE - 1 AND SYSDATE 
-GROUP BY 메뉴ID, 사용유형코드
-HAVING 메뉴ID = 3 AND 사용유형코드 = 100;
-
--- 4
-SELECT 메뉴ID, 사용유형코드, AVG(COUNT(*)) AS AVGCNT
-FROM 시스템사용이력
-GROUP BY 메뉴ID, 사용유형코드;
 
 
